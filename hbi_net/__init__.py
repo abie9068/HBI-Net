9ba1291e65b8fa5cab379180c0628a5127222bb4
abie9068 <niabednuptonmirthaf@gmail.com>
2026-07-18 21:27:36 +0800

    Add files via upload

---
+++ hbi_net/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) HBI-Net authors. All rights reserved.
+"""HBI-Net custom modules for Open-CD.
+
+Importing this package registers the HBI-Net neck, decode head, loss, dataset
+and loading transform into the Open-CD registry so that they can be referred
+to by ``type=...`` in an MMEngine config.
+"""
+from .hbi_net import HBINet
+from .hbi_neck import HBIFusionNeck, HBIBlock
+from .masked_classwise_dice_loss import MaskedClasswiseDiceLoss
+from .sc_decoder import SCDecoderHead
+from .abcd_directional_bloom_dataset import (
+    ABCDDirectionalBloomDataset, MultiImgLoadMSImageFromNpy)
+
+__all__ = [
+    'HBINet',
+    'HBIFusionNeck',
+    'HBIBlock',
+    'MaskedClasswiseDiceLoss',
+    'SCDecoderHead',
+    'ABCDDirectionalBloomDataset',
+    'MultiImgLoadMSImageFromNpy',
+]
